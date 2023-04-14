9c50d353b5c1fcde6190addf6b4ec88764351a0d
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2023-04-13 17:49:48 -0700

    Update images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -1,5 +1,5 @@
 // Auto-generated code. Do not edit.
-namespace myImages {
+namespace myImages2 {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
